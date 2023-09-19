4bd0914bfde6260cf0325213cb15428b642dc438
SerhiiShymko <shimko456@gmail.com>
2023-09-19 12:31:13 +0300

    start

---
+++ src/components/Advertisements/Advertisements.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Advertisements = () => {
+  // Логіка додавання оголошень та список оголошень
+
+  return (
+    <div>
+      {/* Форма додавання оголошення */}
+      {/* Список оголошень */}
+      {/* Деталі оголошення (при кліку) */}
+    </div>
+  );
+};
+
+export default Advertisements;
---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
+
+import Home from './Home/Home';
+import Catalog from './Catalog/Catalog';
+import Advertisements from './Advertisements/Advertisements';
+
+export function App() {
+  return (
+    <Routes>
+     
+        
+        <Route path="/" element={<Home />} />
+        <Route path="catalog" element={<Catalog />} />
+        <Route path="favorites" element={<Advertisements />} />
+      
+    </Routes>
+  );
+}
