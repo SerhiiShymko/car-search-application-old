967ead0f0e29e121b25b6778ffd8f131eb68ce8b
SerhiiShymko <shimko456@gmail.com>
2023-09-22 08:50:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 
-import HomePage from './pages/HomePage/HomePage';
-import CatalogPage from './pages/CatalogPage/CatalogPage';
-import { Layout } from './components/Layout/Layout';
-import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
+// import HomePage from './pages/HomePage/HomePage';
+// import CatalogPage from './pages/CatalogPage/CatalogPage';
+// import { Layout } from './components/Layout/Layout';
+// import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/" element={<Layout />}>
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-      </Route>
-    </Routes>
+    <h1>HEEEEEELLLLLLOO</h1>
+    // <Routes>
+    //   <Route path="/" element={<HomePage />} />
+    //   <Route path="/" element={<Layout />}>
+    //     <Route path="/catalog" element={<CatalogPage />} />
+    //     <Route path="/favorites" element={<FavoritesPage />} />
+    //   </Route>
+    // </Routes>
   );
 }
