641ec827f832edae8b67b897c464bcc54c8c45fa
SerhiiShymko <shimko456@gmail.com>
2023-09-22 08:55:25 +0300

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage/HomePage';
+import CatalogPage from '../pages/CatalogPage/CatalogPage';
+import { Layout } from './Layout/Layout';
+import FavoritesPage from '../pages/FavoritesPage/FavoritesPage';
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Route>
+    </Routes>
+  );
+}
