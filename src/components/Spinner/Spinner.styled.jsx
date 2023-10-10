eb5c1b977dec5ebf5ead93db7b5427d276a6ce6a
SerhiiShymko <shimko456@gmail.com>
2023-10-10 10:57:09 +0300

    Updates

--- src/components/Spinner/Spinner.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const SpinnerStyled = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 1;
-`;
