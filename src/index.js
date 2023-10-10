ab9973f72f720e6606285be1de3d5021ae236498
Serhii SHYMKO <113916099+SerhiiShymko@users.noreply.github.com>
2023-10-10 10:11:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/car-search-application">
+    <BrowserRouter basename="/car-search-application-old">
       <App />
     </BrowserRouter>
   </React.StrictMode>
